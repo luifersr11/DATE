ad373404aea88b432bbc7df8adca5539d62c6989
luifersr11 <121694770+luifersr11@users.noreply.github.com>
2022-12-30 19:46:24 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function (){
+    alert('Me encanto conocerte y este tiempo que hemos estado saliendo ha sido lo mejor de mi viaje, nos vemos el viernes ♥♥')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function (){
+    alert('Decime que día estas libre, para que pase por vos :)')
+});
